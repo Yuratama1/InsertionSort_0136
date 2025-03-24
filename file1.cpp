9d9ad04a6d18355e3cd9f399ef884f7e6e7105a0
Yuratama1 <yuratama661@gmail.com>
2025-03-24 15:36:35 +0700

    Membuat perulangan for untuk menyimpan data

--- file1.cpp
+++ file1.cpp
@@ -7,8 +7,8 @@ int n; // Membuat Variable inputan n
 void input()
 { // Procedure Input
     while (true)
-}
 
+{
 cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah Element Array
 cin >> n; // memanggil variable inputan n
 
@@ -20,9 +20,12 @@ if (n <= 20)
 {
     cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; //Menampilkan pesan jika data lebih dari 20
 }
-{
+}
     cout << endl; // Membuat jarak per baris program
     cout << "=====================" << endl; //Membuat tampilan sususan data elemen array
     cout << "Masukkan Elemnt Array" << endl;
     cout << "=====================" << endl;
-}
+
+    for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada element array
+{
+    cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai data n
