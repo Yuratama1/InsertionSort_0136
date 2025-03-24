f71301717e5a404231448e76d5dd5f850c3b02e9
Yuratama1 <yuratama661@gmail.com>
2025-03-24 15:17:14 +0700

    Membuat inputan element aray

--- file1.cpp
+++ file1.cpp
@@ -2,3 +2,17 @@
 using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang daat 20
+int n; // Membuat Variable inputan n
+
+void input()
+{ // Procedure Input
+    while (true)
+}
+cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah Element Array
+cin >> n; // memanggil variable inputan n
+
+if (n <= 20)
+{ // membuat kondisi n tidak lebih dari 20
+    break;
+}
+else
