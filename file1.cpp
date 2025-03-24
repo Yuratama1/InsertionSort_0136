cca5e0dd66e030d43598ba746ea7c5130dad65e3
Yuratama1 <yuratama661@gmail.com>
2025-03-24 15:28:36 +0700

    Menampilkan pesan data lebih 20

--- file1.cpp
+++ file1.cpp
@@ -8,6 +8,7 @@ void input()
 { // Procedure Input
     while (true)
 }
+
 cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah Element Array
 cin >> n; // memanggil variable inputan n
 
@@ -15,4 +16,13 @@ if (n <= 20)
 { // membuat kondisi n tidak lebih dari 20
     break;
 }
-else
+    else
+{
+    cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; //Menampilkan pesan jika data lebih dari 20
+}
+{
+    cout << endl; // Membuat jarak per baris program
+    cout << "=====================" << endl; //Membuat tampilan sususan data elemen array
+    cout << "Masukkan Elemnt Array" << endl;
+    cout << "=====================" << endl;
+}
