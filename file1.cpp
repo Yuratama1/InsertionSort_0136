bf506e15e78b21207b3602dffb42ed3e2edd4f91
Yuratama1 <yuratama661@gmail.com>
2025-03-24 15:53:01 +0700

    Membuat Outputnya

--- file1.cpp
+++ file1.cpp
@@ -8,24 +8,47 @@ void input()
 { // Procedure Input
     while (true)
 
-{
-cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah Element Array
-cin >> n; // memanggil variable inputan n
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah Element Array
+        cin >> n; // memanggil variable inputan n
 
-if (n <= 20)
-{ // membuat kondisi n tidak lebih dari 20
-    break;
-}
-    else
-{
-    cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; //Menampilkan pesan jika data lebih dari 20
-}
-}
-    cout << endl; // Membuat jarak per baris program
-    cout << "=====================" << endl; //Membuat tampilan sususan data elemen array
-    cout << "Masukkan Elemnt Array" << endl;
-    cout << "=====================" << endl;
+        if (n <= 20)
+        { // membuat kondisi n tidak lebih dari 20
+            break;
+        }
+            else
+            {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; //Menampilkan pesan jika data lebih dari 20
+            }
+    }
+        cout << endl; // Membuat jarak per baris program
+        cout << "=====================" << endl; //Membuat tampilan sususan data elemen array
+        cout << "Masukkan Elemnt Array" << endl;
+        cout << "=====================" << endl;
 
     for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada element array
-{
+    {
     cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai data n
+    cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
+    }
+}
+
+void insertionSort()
+{// Procedure InsertionSort
+    int temp; //Membuat variable data temporer atau penyimpanan sementara
+    int j, i; //Membuat variable j sebagai penanda
+
+    for (i = 1; i <= - 1; i++)
+    {//Step 1
+        temp = arr[i]; //Step 2
+
+        j = i - 1; //Step 3
+
+        while (j >= 0 && arr[j] > temp) //Step 4
+        {
+        arr[j + 1] = arr[j]; //Step 4a
+        j--; //step 4b
+        }
+        arr[j + 1] = temp; //Step 5
+    }
+}
